eab6f7d50472177ab59622f31d4eb469e1539c3a
Rashid Alam <alamm2412@gmail.com>
2022-11-15 16:25:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import {Box, Container,VStack} from "@chakra-ui/react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Box>
+    <Container h={"100vh"} bg={"red"}>
+       rashid
+    </Container>
+  </Box>
 }
 
 export default App;
