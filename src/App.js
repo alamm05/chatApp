8422ed286cdc5e431d939e735dfa53bfa1522374
Rashid Alam <alamm2412@gmail.com>
2022-11-15 17:41:10 +0530

    :second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import {Box, Container,VStack} from "@chakra-ui/react";
+import {Box,Button,Input, Container,VStack,HStack} from "@chakra-ui/react";
 
 
 function App() {
-  return <Box>
-    <Container h={"100vh"} bg={"red"}>
-       rashid
+  return <Box bg={"red.50"}>
+    <Container h={"100vh"} bg={"white"}>
+       <VStack h={"full"} paddingY={"3"}>
+         <Button colorScheme={"red"} w={"full"}>
+           Logout
+         </Button>
+
+         <VStack  h={"full"} w={"full"}></VStack>
+
+         <form style={{w:"100%"}}>
+          <HStack>
+          <Input placeholder="Enter a message..." />
+          <Button colorScheme={"purple"} type="Submit">Send</Button>
+          </HStack>
+         </form>
+       </VStack>
     </Container>
   </Box>
 }
