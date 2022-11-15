6dfc4674c2013731a3fc998624263042e4b057d4
Rashid Alam <alamm2412@gmail.com>
2022-11-15 23:08:19 +0530

    :third commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import {Box,Button,Input, Container,VStack,HStack} from "@chakra-ui/react";
+import Message from "./Components/Message";
+import { useEffect, useState } from "react";
+import {onAuthStateChanged,  getAuth, GoogleAuthProvider,signInWithPopup,signOut} from "firebase/auth";
+import {app} from "./firebase";
 
+
+const auth = getAuth(app)
+
+const loginHandler =()=>{
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth,provider);
+}
+
+const logoutHandler=()=>{
+  signOut(auth);
+}
 
 function App() {
+  const [user, SetUser] =useState(false);
+  useEffect (()=>{
+     const unsubscribe = onAuthStateChanged(auth,(data)=>{
+      SetUser(data);
+    });
+
+    //return statement is used for unmounting
+   return ()=>{
+    unsubscribe();
+   }
+  })
+ 
   return <Box bg={"red.50"}>
-    <Container h={"100vh"} bg={"white"}>
+    {
+      user ? (
+        <Container h={"100vh"} bg={"white"}>
        <VStack h={"full"} paddingY={"3"}>
-         <Button colorScheme={"red"} w={"full"}>
+         <Button onClick = {logoutHandler} colorScheme={"red"} w={"full"}>
            Logout
          </Button>
 
-         <VStack  h={"full"} w={"full"}></VStack>
-
+         <VStack  h={"full"} w={"full"} overflowY ="auto">
+           <Message text={"sample message"}/>
+           <Message user ="me" text={"sample message"}/>
+           <Message text={"sample message"}/>
+         </VStack>
+         
          <form style={{w:"100%"}}>
           <HStack>
           <Input placeholder="Enter a message..." />
@@ -19,6 +52,10 @@ function App() {
          </form>
        </VStack>
     </Container>
+      ) : <VStack  bg ="white" justifyContent ={"center"} h="100vh">
+        <Button onClick ={loginHandler} height ={"1.5rem"} width={"10rem"}colorScheme={"purple"}>Sign In With Google</Button>
+      </VStack>
+    }
   </Box>
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHuUKJXBmW2upF5G87zaLYqlvSOYIrb94",
+  authDomain: "chatapp-1c112.firebaseapp.com",
+  projectId: "chatapp-1c112",
+  storageBucket: "chatapp-1c112.appspot.com",
+  messagingSenderId: "435285161035",
+  appId: "1:435285161035:web:68951f6ba89424c0ec89ce",
+  measurementId: "G-2Y3NGDR75T"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
